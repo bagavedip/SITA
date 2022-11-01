040c437560f58959bd8fd769a42767ca5e416332
dipika <bagavedipika1423@gmail.com>
2022-11-01 10:39:24 +0530

    users models changes according to dev server code

--- sita/constants/perspective_constants.py
+++ sita/constants/perspective_constants.py
@@ -8,8 +8,8 @@ PERSPECTIVE_TABLE_HEADER = {
     'INCIDENT ID': 'id',
     'Publish / Draft': 'is_published',
     'DATE': 'created_at',
-    'PERSPECTIVE TYPE': 'perspective_type',
-    'PERSPECTIVE  TITLE': 'perspective_title',
+    'TYPE': 'perspective_type',
+    'TITLE': 'perspective_title',
     'STATUS TYPE': 'status_type',
     'ACTION TYPE': 'action_type'
 }
